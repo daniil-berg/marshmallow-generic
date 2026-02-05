c65daaf4e9368dac5388e10fd3e32da0110ef21d
Daniel Fainberg <mail@daniil.fajnberg.de>
2026-02-05 14:12:20 +0100

    ➖ Remove `black`/`isort` dev dependencies in favor of `ruff`
    Adapt project config and adjust scripts accordingly.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,13 +5,7 @@ source "$(dirname $(realpath $0))/util.sh"
 
 echo 'Linting source and test files...'
 
-echo '  isort - consistent imports'
-isort src/ tests/ --check-only
-
-echo '  ruff - extensive linting'
 ruff check src/ tests/
-
-echo '  black - consistent style'
-run_and_capture black src/ tests/ --check
+ruff format --check src/ tests/
 
 echo -e "${bold_green}No issues found${color_reset}\n"
--- scripts/util.sh
+++ scripts/util.sh
@@ -1,13 +1,3 @@
-run_and_capture() {
-  # Captures stderr of any command passed to it
-  # and releases it only if the command exits with a non-zero code.
-  typeset output
-  output=$($@ 2>&1)
-  typeset exit_status=$?
-  [[ $exit_status == 0 ]] || >&2 echo "${output}"
-  return $exit_status
-}
-
 # Ensure that we return to the current working directory
 # and exit the script immediately in case of an error:
 trap "cd $(realpath ${PWD}); exit 1" ERR
