b4dd0ad445e3b5c924ed0efac9d11a0a927fbc75
Daniel Fainberg <mail@daniil.fajnberg.de>
2026-02-06 10:19:31 +0100

    🔨 Fix broken coverage script

--- scripts/cov.sh
+++ scripts/cov.sh
@@ -6,5 +6,10 @@
 source "$(dirname $(realpath $0))/util.sh"
 
 coverage erase
-run_and_capture coverage run
+# Capture the test progression in a variable:
+typeset progress
+progress=$(coverage run 2>&1)
+# If tests failed or produced errors, write progress/messages to stderr and exit:
+[[ $? -eq 0 ]] || { >&2 echo "${progress}"; exit 1; }
+# Otherwise extract the total coverage percentage from the produced report and write it to stdout:
 coverage report | awk '$1 == "TOTAL" {print $NF; exit}'
